3bd014ed703d4a907f99af21384cdc5dfc2a798d
Rob Sanchez <rob@robsanchez.com>
2014-11-08 10:15:56 -0600

    First commit

---
+++ eecli.rb
@@ -0,0 +1,35 @@
+require 'formula'
+require File.expand_path("../../Requirements/php-meta-requirement", __FILE__)
+require File.expand_path("../../Requirements/phar-requirement", __FILE__)
+
+class Eecli < Formula
+  homepage 'https://github.com/rsanchez/eecli'
+  url 'https://github.com/rsanchez/eecli/releases/download/0.0.0-alpha/eecli.phar'
+  sha1 '7d1dfd1f7da34c9a1d266a1608ffaf48d2f808be'
+  version '0.0.0-alpha'
+
+  depends_on PhpMetaRequirement
+  depends_on PharRequirement
+
+  def install
+    libexec.install "eecli.phar"
+    sh = libexec + "eecli"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/eecli.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  test do
+    system 'eecli --version'
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "eecli --version".
+
+    You can read more about eecli by running:
+      "brew home eecli".
+    EOS
+  end
+
+end
