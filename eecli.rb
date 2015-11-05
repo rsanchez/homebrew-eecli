c826853d1fb641d251dad3253e226b877d56339e
Rob Sanchez <rob@robsanchez.com>
2015-11-04 20:16:49 -0600

    Release 1.0.9

--- eecli.rb
+++ eecli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Eecli < Formula
   homepage 'https://github.com/rsanchez/eecli'
-  url 'https://github.com/rsanchez/eecli/releases/download/1.0.8/eecli.phar'
-  sha1 '248faf145d2ff5f71de55d2d24d293a1e6773017'
-  version '1.0.8'
+  url 'https://github.com/rsanchez/eecli/releases/download/1.0.9/eecli.phar'
+  sha1 'ebf7f382376996e060aa6d32e1fd50618705968b'
+  version '1.0.9'
 
   def install
     libexec.install "eecli.phar"
